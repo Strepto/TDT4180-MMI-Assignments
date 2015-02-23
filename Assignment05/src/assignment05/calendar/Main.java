1d658d19a06d103167c97f44679ef4c6607201ec
Nils Henrik Hals <nilshenrik@minorplanet.no>
2015-02-23 18:29:26 +0100

    - Added icon

--- Assignment05/src/assignment05/calendar/Main.java
+++ Assignment05/src/assignment05/calendar/Main.java
@@ -3,6 +3,7 @@ package assignment05.calendar;
 import javafx.application.Application;
 import javafx.stage.Stage;
 import javafx.scene.Scene;
+import javafx.scene.image.Image;
 import javafx.scene.layout.BorderPane;
 import javafx.fxml.FXMLLoader;
 
@@ -15,6 +16,8 @@ public class Main extends Application {
 			Scene scene = new Scene(root,root.getHeight(),root.getWidth());
 			scene.getStylesheets().add(getClass().getResource("application.css").toExternalForm());
 			primaryStage.setScene(scene);
+			primaryStage.setTitle("Calendar");
+			primaryStage.getIcons().add(new Image("file:icon_32.png")); //"img/ic_action_go_to_today.png"
 			primaryStage.show();
 		} catch(Exception e) {
 			e.printStackTrace();
